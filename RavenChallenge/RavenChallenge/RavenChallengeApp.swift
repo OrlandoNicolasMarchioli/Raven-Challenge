25cdfb2167b374c62716449e2ea570766c618c8d
Nicolas <nico.marchioli@gmail.com>
2024-07-08 14:50:03 -0300

    Clean architecture added

---
+++ RavenChallenge/RavenChallenge/RavenChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RavenChallengeApp.swift
+//  RavenChallenge
+//
+//  Created by Nico on 08/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct RavenChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
